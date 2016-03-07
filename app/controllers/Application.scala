5b355ab7c3b9397c85e1115ad79893cfdd9d5c6a
ioscar <ioscar@chalmers.se>
2016-03-07 08:53:16 +0100

    Init filter parser.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -3,43 +3,31 @@ package controllers
 import play.api._
 import play.api.mvc._
 import play.api.libs.json._
-//import play.api.libs.functional.syntax._
 import play.api.libs.ws._
-//import play.api.libs.ws.ning.NingAsyncHttpClientConfigBuilder
 import play.api.Play.current
 import play.api.libs.concurrent.Execution.Implicits._
 
 import scala.concurrent.{Await, Future}
+import scala.util.matching.Regex
 
 class Application extends Controller {
 
-	// Get configuration settings
+	/*
+	 *	Configuration variables
+	 */
+
 	val es_host = Play.current.configuration.getString("es.host").get
 	val es_index = Play.current.configuration.getString("es.index").get
+	val es_user = Play.current.configuration.getString("es.username").get
+	val es_pw = Play.current.configuration.getString("es.password").get
 	
+	/*
+	 *	Actions
+	 */
+
 	// Index page
 	def index = Action {
 		Ok(views.html.index("Your new application is ready."))
-	}
-
-	def test = Action.async {
-
-		Future {
-			Thread.sleep(2000)
-			"tjenare mannen"
-		}.map {
-			res => Ok(res)
-		}
-
-		/*
-		val ok = Ok("Hello world!")
-		val notFound = NotFound
-		val pageNotFound = NotFound(<h1>Page not found</h1>)
-		val badRequest = BadRequest(views.html.form(formWithErrors))
-		val oops = InternalServerError("Oops")
-		val anyStatus = Status(488)("Strange response type")
-		*/
-
 	}
 
 	// Get information from ES index about types and counts.
@@ -57,47 +45,44 @@ class Application extends Controller {
 			.recover {
 				case e: Throwable => BadRequest("Bad request!")
 			}
-
-	}
-
-
-	def get_motioner_all() = Action {
-		print("get motioner all")
-		Ok("Get all motioner")
 	}
 
 	
-	def get_motioner_from_keywords(tags: String) = Action.async {
+	def get_motioner_timeline(search_phrase: String) = Action.async {
 
-		
+		// Set variables
 		val es_type = Play.current.configuration.getString("es.type.motioner").get
 
+		// Parse search string
+		val parsed = query_parser(search_phrase)
+
+		val match_tags: List[String] = search_phrase.split(",").map(_.trim).toList
+		println(match_tags)
+
+		Future {
+			Thread.sleep(1000)
+			"tjenare mannen"
+		}.map {
+			res => Ok(res)
+		}
+
 		/*
-		if (tags.isEmpty) {
-			Redirect(routes.Application.get_motioner_all())
-		} else {
-		*/
-
-		val match_tags: List[String] = tags.split(",").map(_.trim).toList
-
+		// Create ES queries
 		val data = Json.obj(
 			"query" -> Json.obj(
 				"match" -> Json.obj(
-					"dokument.html.html_filtered" -> match_tags.mkString(" ")
+					"dokument.html" -> match_tags.mkString(" ")
 				)
-			),
-			"fields" -> Json.arr(
-				"dokument.dokument_url_html"
 			)
 		)
 
 		println(data)
 
+		// Query ES
 		WS.url(es_host + es_index + "/" + es_type + "/_search")
+			.withAuth(es_user, es_pw, WSAuthScheme.BASIC)
 			.post(data)
 			.map { response =>
-				//println(response.status)
-				//println(response.body)
 				if (response.status == 200) {
 					Ok(response.json)
 				} else {
@@ -107,9 +92,56 @@ class Application extends Controller {
 			.recover {
 				case e: Throwable => BadRequest("Bad request!")
 			}
-
+		*/
 	}
 	
+	/*
+	 *	Help Functions
+	 */
+
+	def query_parser(search_phrase: String) : String = {
+
+		println(" ")
+		println(search_phrase)
+
+		val str = "Scala is scalable and cool try:(test, asdf) filter:(1244-2345)"
+
+		// Extract filter from search phrase
+		var pattern = "(\\S*?):\\(.*?\\)".r
+		val filter_extractions = pattern.findAllIn(str)
+		val remaining_terms = pattern.replaceAllIn(str, "")
+
+		println(remaining_terms)
+
+		// Parse filter terms
+		for(filter <- filter_extractions) {
+
+			var key: Option[String] = "(.*?)(?=:)".r.findFirstIn(filter.trim)
+			var filter_params: Option[String] = "(?<=\\()(.*?)(?=\\))".r.findFirstIn(filter.trim)
+
+			// If filter key and params are found
+			if (key != None && filter_params != None) {
+				println(key.get)
+				println(filter_params.get)
+
+				// Check if filter params contain year segement or string filters
+				if (filter_params.get.contains("-")) {
+					var start_year: Option[String] = "(\\d*?)(?=\\-)".r.findFirstIn(filter_params.get)
+					var end_year: Option[String] = "(?<=\\-)(\\d*?)($|\\,|\\s)".r.findFirstIn(filter_params.get)
+					
+					if (start_year != None && end_year != None) {
+						println(start_year.get.toInt)
+						println(end_year.get.toInt)
+					}
+
+				} else {
+
+				}
+			}
+		}
+
+		return "test"
+	}
 
 }
 
@@ -118,6 +150,32 @@ class Application extends Controller {
 
 
 /*
+
+def test = Action.async {
+
+	Future {
+		Thread.sleep(2000)
+		"tjenare mannen"
+	}.map {
+		res => Ok(res)
+	}
+
+	/*
+	val ok = Ok("Hello world!")
+	val notFound = NotFound
+	val pageNotFound = NotFound(<h1>Page not found</h1>)
+	val badRequest = BadRequest(views.html.form(formWithErrors))
+	val oops = InternalServerError("Oops")
+	val anyStatus = Status(488)("Strange response type")
+	*/
+
+}
+
+
+def get_motioner_all() = Action {
+	print("get motioner all")
+	Ok("Get all motioner")
+}
 
 def testRequest = Action.async {
 
--- public/javascripts/hello.js
+++ public/javascripts/hello.js
@@ -8,15 +8,24 @@ if (window.console) {
 	http_req.onreadystatechange = function() {
 		if (http_req.readyState === XMLHttpRequest.DONE) {
 			if (http_req.status === 200) {
-				console.log(JSON.parse(http_req.responseText));
-				//console.log(http_req.responseText);
+				//console.log(JSON.parse(http_req.responseText));
+				console.log(http_req.responseText);
 			} else {
 				console.log(http_req.responseText);
 			}
 		}
 	}
 
-	http_req.open('GET', 'http://0.0.0.0:9000/motioner?tags=hej,test');
-	http_req.send();
+	test_terms = [
+		'asdf',
+		'asdf,qwer,zxcv parti:(m,s) år:(1995-2000)',
+		'mer .* till författare:(anders borg)',
+		'mer pengar till .*'
+	]
+
+	test_terms.forEach(function(entry) {
+		http_req.open('GET', 'http://0.0.0.0:9000/motioner?search_phrase=' + entry, false);
+		http_req.send();
+	});
 
 }
